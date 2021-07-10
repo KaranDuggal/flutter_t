8cde71ee645edba27a006a2e0e0581f0e1ee1fc7
KaranDuggal <duggalkaran400@gmail.com>
2021-07-10 18:46:09 +0530

    day 6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:d_stor/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = '';
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -16,7 +23,7 @@ class LoginPage extends StatelessWidget {
                 fit: BoxFit.cover,
               ),
               SizedBox(height: 20.0),
-              Text("Welcome D-Stor",
+              Text("Welcome D-Stor $name",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               SizedBox(height: 20.0),
               Padding(
@@ -24,24 +31,56 @@ class LoginPage extends StatelessWidget {
                     vertical: 16.0, horizontal: 32.0),
                 child: Column(children: [
                   TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter UserName", labelText: "UserName")),
+                    decoration: InputDecoration(
+                        hintText: "Enter UserName", labelText: "UserName"),
+                    onChanged: (value) {
+                      name = value;
+                      print(name);
+                      setState(() {});
+                    },
+                  ),
                   SizedBox(height: 20.0),
                   TextFormField(
                       obscureText: true,
                       decoration: InputDecoration(
                           hintText: "Enter Password", labelText: "Password")),
                   SizedBox(height: 20.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      print("duggal");
+                  InkWell(
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changeButton ? 100 : 150,
+                      height: 50,
+                      // color:Colors.blueAccent[100],
+                      alignment: Alignment.center,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                      decoration: BoxDecoration(
+                          color: Colors.blueAccent[100],
+                          borderRadius: BorderRadius.circular(8)),
+                    ),
+                    onTap: () async{
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds:1));
                       Navigator.pushNamed(context, Routes.homeRoute);
                     },
-                    child: Text('Login'),
-                    style: TextButton.styleFrom(
-                      minimumSize:Size(150,40)
-                    )
-                  )
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     print("duggal");
+                  //     Navigator.pushNamed(context, Routes.homeRoute);
+                  //   },
+                  //   child: Text('Login'),
+                  //   style: TextButton.styleFrom(
+                  //     minimumSize:Size(150,40)
+                  //   )
+                  // )
                 ]),
               ),
             ],
