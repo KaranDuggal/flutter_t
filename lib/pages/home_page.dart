4821e64f9cbd4f216e8c6aed8da8716282a2b37d
KaranDuggal <duggalkaran400@gmail.com>
2021-07-13 14:02:55 +0530

    day 13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,17 +1,44 @@
+import 'dart:convert';
+
 import 'package:d_stor/models/catalog.dart';
 import 'package:d_stor/widgets/drawer.dart';
 import 'package:d_stor/widgets/items_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final days = 40;
+
   final name = 'duggal';
 
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productData = decodeData["products"];
+    // List<Item> list = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(productData).map<Item>((item) => Item.fromMap(item)).toList();
+    setState(() {
+      
+    });
+    // print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
+    // final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         // backgroundColor: Colors.white,
@@ -21,15 +48,17 @@ class HomePage extends StatelessWidget {
       drawer: MyDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
+        child: (CatalogModel.items.isNotEmpty) ? ListView.builder(
           // itemCount: CatalogModel.items.length,
-          itemCount: dummyList.length,
+          itemCount: CatalogModel.items.length,
           itemBuilder: (BuildContext context, int index) {
           return ItemWidget(
-            item: dummyList[index],
+            item: CatalogModel.items[index],
             // item: CatalogModel.items[index],
             );
           },
+        ):Center(
+          child: CircularProgressIndicator()
         ),
       )
     );
